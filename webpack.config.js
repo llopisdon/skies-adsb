54a6fece51df8232491364483dea0733f10f042f
Don E. Llopis <llopis.don@gmail.com>
2021-10-16 09:24:50 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require("path")
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/script.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      verbose: true
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './static'),
+          noErrorOnMissing: true,
+        }
+      ],
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html'
+    }),
+  ],
+  devServer: {
+    open: true
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+}
